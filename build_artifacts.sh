1d5c2e16a99cec6b183189a3220711d5a48c3768
Aaron Krill <aaron@krillr.com>
2014-08-29 15:57:15 +0000

    proper args to yuicompressor

--- build_artifacts.sh
+++ build_artifacts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp sonrai.js $CIRCLE_ARTIFACTS
 wget https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar
-java -jar yuicompressor-2.4.8.jar -t js sonrai.js > $CIRCLE_ARTIFACTS/sonrai.min.js
+java -jar yuicompressor-2.4.8.jar --type js sonrai.js > $CIRCLE_ARTIFACTS/sonrai.min.js
