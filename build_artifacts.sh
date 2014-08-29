88307284b8447ac7d55befdd302d18a23f787f3a
Aaron Krill <aaron@krillr.com>
2014-08-29 15:55:25 +0000

    artifacts?

---
+++ build_artifacts.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp sonrai.js $CIRCLE_ARTIFACTS
+wget https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar
+java -jar yuicompressor-2.4.8.jar -t js sonrai.js > $CIRCLE_ARTIFACTS/sonrai.min.js
