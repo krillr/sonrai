77ca05cc357b0dd7aebf1fd4984628f856e80c1f
Aaron Krill <aaron@krillr.com>
2014-09-07 13:28:41 +0000

    missing build artificats script changes

--- build_artifacts.sh
+++ build_artifacts.sh
@@ -2,3 +2,4 @@
 cp sonrai.js $CIRCLE_ARTIFACTS
 wget https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar
 java -jar yuicompressor-2.4.8.jar --type js sonrai-bundle.js > $CIRCLE_ARTIFACTS/sonrai.min.js
+mv coverage $CIRCLE_ARTIFACTS
