7b27677fa4b070acb07f8363c4ded6c16cfd85fd
Aaron Krill <aaron@krillr.com>
2014-09-08 11:05:12 +0000

    meh

--- build_artifacts.sh
+++ build_artifacts.sh
@@ -2,3 +2,4 @@
 wget --quiet https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar
 java -jar yuicompressor-2.4.8.jar --type js sonrai-bundle.js > sonrai.min.js
 rm yuicompressor-2.4.8.jar
+mv sonrai.min.js coverage $CIRCLE_ARTIFACTS/
